fa733f700969e6b544358c8b7c17122ee9ee0848
Arya Soni <aryasoni98@gmail.com>
2022-12-24 21:08:42 +0530

    Update

--- components/rightSidebar/SpacingContent.tsx
+++ components/rightSidebar/SpacingContent.tsx
@@ -1,6 +1,7 @@
+// code to remove non-numeric characters from text
+
 import { useComponentStore } from '@/store/useComponentsStore';
 import { Button, Flex, Input, Text } from 'native-base';
-import React, { useEffect } from 'react';
 
 export default function SpacingContent() {
   const {updateStyles,globalStyles, editComponents,components} = useComponentStore();
@@ -8,12 +9,12 @@ export default function SpacingContent() {
   return (
     <Flex p={2} justifyContent='space-between'>
       <Text>Margin</Text>
-      <Input placeholder='All' type='number' onChangeText={(e) => {
+      <Input placeholder='All' onChangeText={(e) => {
          updateStyles({ m: e })
           editComponents(0, <Button {...globalStyles} m={e} >Button</Button>)
         }} />
       <Text> Padding </Text>
-      <Input placeholder='All' type='number' onChangeText={(e) => {
+      <Input placeholder='All' onChangeText={(e) => {
          updateStyles({ p: e })
           editComponents(0, <Button {...globalStyles} p={e} >Button</Button>)
         }} />
