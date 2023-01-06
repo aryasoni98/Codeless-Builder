201d8b6fa8bae751c4feead7b301ceab0e82a00c
Arya Soni <aryasoni98@gmail.com>
2023-01-07 02:54:09 +0530

    Added cypress

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  projectId: "5nfdod",
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
+};
